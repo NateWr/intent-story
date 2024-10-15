69539bd951f46c73ffd98724bbc834ea089cc5ef
Nate Wright <natew@notthisway.com>
2024-10-15 13:49:07 +0100

    Add content for end chapter

--- src/components/chapters/ChapterEnd.vue
+++ src/components/chapters/ChapterEnd.vue
@@ -4,7 +4,7 @@ import Chapter from '../Chapter.vue'
 import Narration from '../Narration.vue'
 import PositionedContent from '../PositionedContent.vue'
 import Quote from '../Quote.vue'
-import CityIntro from './CityIntro.vue'
+import CityEnd from './CityEnd.vue'
 import type { I18N } from '../../types/i18n'
 
 defineProps({
@@ -30,36 +30,138 @@ defineProps({
     }"
   >
     <template #back>
-      <PositionedContent :left="(50 * scale)">
-        <Quote
-          name="Pulvinar Ullamcorper"
-          role="Ex Accumsan Nunc"
-          date="Oct 13, 2023"
-          :offsetBottom="`${300 * scale}px`"
-        >
-          “Dolor torquent lacinia class <strong>litora pulvinar</strong> velit fusce tristique.”
-        </Quote>
-      </PositionedContent>
-      <PositionedContent :left="(2000 * scale)">
+      <PositionedContent :left="(1011 * scale)">
         <Narration
           size="lg"
           :offsetBottom="`${150 * scale}px`"
         >
           <p>
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Israeli leaders revealed their genocidal intent through colonial tropes, racialized language, and the use of dehumanizing terms to describe <strong>Palestinians as a whole</strong>.
           </p>
         </Narration>
       </PositionedContent>
-      <PositionedContent :left="(3500 * scale)">
-        <Narration
-          :offsetBottom="`${0 * scale}px`"
+      <PositionedContent :left="(3053 * scale)">
+        <Quote
+          name="Benjamin Netanyahu"
+          role="Israeli Prime Minister"
+          date="Oct 25, 2023"
+          :offsetBottom="`${200 * scale}px`"
         >
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Montes pellentesque praesent pellentesque rutrum ut. Ornare dolor vitae eu torquent natoque taciti enim vivamus. Commodo natoque sollicitudin posuere vehicula taciti taciti primis vehicula.</p>
+          “We are the people of the light, they are the <strong>people of darkness</strong>.”
+        </Quote>
+      </PositionedContent>
+      <PositionedContent :left="(4833 * scale)">
+        <Narration :offsetBottom="`${100 * scale}px`">
+          <CallAndResponse
+            ref="school"
+            :line="false"
+            :isVisible="false"
+          >
+            <div class="car-call">
+              Their rhetoric was so alarming that 800 scholars of genocide studies and international law immediately spoke out.
+            </div>
+            <blockquote class="car-response">
+              <p>
+                “Language used... appears to reproduce rhetoric and tropes associated with genocide and incitement to genocide.”
+              </p>
+              <cite class="car-response-cite">15 Oct 2023</cite>
+            </blockquote>
+          </CallAndResponse>
+        </Narration>
+      </PositionedContent>
+      <PositionedContent :left="(6634 * scale)">
+        <Narration
+          size="lg"
+          :offsetBottom="`${150 * scale}px`"
+        >
+          <p>
+            Their warning was <strong>born out</strong> in the months of bombing, invasion, starvation, and mass displacement unleashed by Israel.
+          </p>
+        </Narration>
+      </PositionedContent>
+      <PositionedContent :left="(8723 * scale)">
+        <Narration
+          :offsetBottom="`${150 * scale}px`"
+        >
+          <p>
+            On January 26th, the International Court of Justice ordered “<strong>immediate and effective measures</strong>” to protect Palestinians in Gaza from the risk of genocide.
+          </p>
+          <p>
+            Israeli leaders ignored the demand and <strong>belittled the court</strong>.
+          </p>
+        </Narration>
+      </PositionedContent>
+      <PositionedContent :left="(10295 * scale)">
+        <Quote
+          name="Yoav Gallant"
+          role="Minister of Defense"
+          date="Jan 26, 2024"
+          :offsetBottom="`${200 * scale}px`"
+        >
+          “Whoever is looking for justice, will not find it on the leather chairs in The Hague.”
+        </Quote>
+      </PositionedContent>
+      <PositionedContent :left="(11788 * scale)">
+        <Narration
+          size="lg"
+          :offsetBottom="`${150 * scale}px`"
+        >
+          <p>
+            A month later, human rights groups confirmed what everyone knew: <strong>Israel was defying the court.</strong>
+          </p>
+          <p>
+            It continued to obstruct aid, commit war crimes, and use starvation as a weapon of war.
+          </p>
+        </Narration>
+      </PositionedContent>
+      <PositionedContent :left="(13621 * scale)">
+        <Quote
+          name="Rami Igra"
+          role="Former Head, Mossad Hostages and MIA Unit"
+          date="Feb 14, 2024"
+          :offsetBottom="`${150 * scale}px`"
+        >
+          “There are no uninvolved people in Gaza... everyone is involved.”
+        </Quote>
+      </PositionedContent>
+      <PositionedContent :left="(15448 * scale)">
+        <Narration
+          :offsetBottom="`${100 * scale}px`"
+        >
+          <p>
+            As Israel's genocide drags on, it has become increasingly difficult to count the dead.
+          </p>
+          <p>
+            Gaza's Ministry of Health reported 40,861 fatalities as of September 4, 2024, but experts project the direct and indirect death toll will be many times higher.
+          </p>
+        </Narration>
+      </PositionedContent>
+      <PositionedContent :left="(17290 * scale)">
+        <Narration
+          size="lg"
+          :offsetBottom="`${200 * scale}px`"
+        >
+          <p>
+            In the last year, Israel has shown the intent, capacity and determination to permanently extinguish the <strong>conditions of life</strong> in Gaza.
+          </p>
+        </Narration>
+      </PositionedContent>
+      <PositionedContent :left="(19121 * scale)">
+        <Narration
+          :offsetBottom="`${100 * scale}px`"
+        >
+          <p>
+            They will not stop without a broad campaign of boycotts, divestment and sanctions that inhibit their ability to continue this genocide.
+          </p>
+          <p>
+            Yet the United States, Germany, and other allies continue to provide Israel's genocide with military, political and diplomatic protection. In doing so, they defy their own treaty obligations to prevent another genocide.
+          </p>
         </Narration>
       </PositionedContent>
     </template>
-    <CityIntro :scale="scale" />
+
+    <CityEnd :scale="scale" />
+
   </Chapter>
 </template>
 
---
+++ src/components/chapters/CityEnd.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps({
+  scale: Number,
+})
+</script>
+
+<template>
+  <svg :width="(20834 * scale).toFixed()" :height="(600 * scale).toFixed(0)" viewBox="0 0 20834 600" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <use xlink:href="#svg-buildings" :transform="`translate(109, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(1378, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(2647, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(3187, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(4487, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(5820, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(6454, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(7723, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(8992, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(10261, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(12279, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(14068, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(15337, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(16606, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(17875, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(18212, ${600-330})`" width="1432" height="330"/>
+    <use xlink:href="#svg-buildings" :transform="`translate(19397, ${600-330})`" width="1432" height="330"/>
+    <path d="M13919.7 496.686L13827.1 471L13794.1 514.784L13677.5 529.962L13623 579H14366L14229.9 514.784L14027.3 471L13919.7 496.686Z" fill="var(--slate-light)"/>
+    <path d="M13421.7 496.686L13329.1 471L13296.1 514.784L13179.5 529.962L13125 579H13868L13731.9 514.784L13529.3 471L13421.7 496.686Z" fill="var(--slate-light)"/>
+    <path d="M12923.7 496.686L12831.1 471L12798.1 514.784L12681.5 529.962L12627 579H13370L13233.9 514.784L13031.3 471L12923.7 496.686Z" fill="var(--slate-light)"/>
+    <path d="M12273.3 496.686L12365.9 471L12398.9 514.784L12515.5 529.962L12570 579H11827L11963.1 514.784L12165.7 471L12273.3 496.686Z" fill="var(--slate-light)"/>
+    <path d="M11868.3 496.686L11960.9 471L11993.9 514.784L12110.5 529.962L12165 579H11422L11558.1 514.784L11760.7 471L11868.3 496.686Z" fill="var(--slate-light)"/>
+    <path d="M12551.3 496.686L12643.9 471L12676.9 514.784L12793.5 529.962L12848 579H12105L12241.1 514.784L12443.7 471L12551.3 496.686Z" fill="var(--slate-light)"/>
+  </svg>
+</template>
