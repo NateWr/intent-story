62c197a99afd3d90ff2450e4de782dc1e964b84a
Nate Wright <natew@notthisway.com>
2024-12-04 10:41:35 +0000

    Remove foreground image covering title in Starvation chapter

--- src/components/chapters/CityStarvationFront.vue
+++ src/components/chapters/CityStarvationFront.vue
@@ -34,6 +34,5 @@ const scaledWidth = computed(() => props.width * props.speed)
     <image id="city-dark-4" href="city-dark-4.png" width="816" height="566" :x="speed * 5677" y="385" />
     <image id="city-dark-3" href="city-dark-3.png" width="535" height="477" :x="speed * 16484" y="430" />
     <image id="city-dark-5" href="city-dark-5.png" width="369" height="653" :x="speed * 19236" y="270" />
-    <image id="city-dark-5" href="city-dark-5.png" width="643" height="1138" :x="speed * 467" y="-205" />
   </svg>
 </template>
